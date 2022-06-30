7bdbd696ff3ff2c1aad64246374750fc9a2647e4
hreddy <harishreddy616@gmail.com>
2022-07-01 01:21:07 +0530

    place holders

---
+++ src/services/WeatherService.js
@@ -0,0 +1,24 @@
+export async function getWeatherInfo() {
+    try{
+        //Make API call to get the weather info 
+        /*
+        const response = await fetch('/api/');
+        return await response.json();
+        */
+    }catch(error)
+    {
+        return [];
+    }
+}
+export async function saveWeatherInfo() {
+    try{
+        //Make API call to save the weather info 
+        /*
+        const response = await fetch('/api/');
+        return await response.json();
+        */
+    }catch(error)
+    {
+        return [];
+    }
+}
