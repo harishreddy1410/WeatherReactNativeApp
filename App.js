6e2ec177921f5895f6c8f25e1d1c52c27dc54528
hreddy <harishreddy616@gmail.com>
2022-07-01 10:24:05 +0530

    added a react component for displaying the weather detail

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  WeatherDetail  from './src/components/WeatherDetail'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Harish!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+       <WeatherDetail name="Winter"></WeatherDetail> 
       <StatusBar style="auto" />
     </View>
   );
